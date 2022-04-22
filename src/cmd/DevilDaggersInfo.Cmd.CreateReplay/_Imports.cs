e5b74c1ba4319ddd25967752a30f8c4d3e15b268
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-04-22 13:02:57 +0200

    Move projects to subdirectories

---
+++ src/cmd/DevilDaggersInfo.Cmd.CreateReplay/_Imports.cs
@@ -0,0 +1,5 @@
+global using DevilDaggersInfo.Core.Replay;
+global using DevilDaggersInfo.Core.Replay.Enums;
+global using DevilDaggersInfo.Core.Replay.Events;
+global using DevilDaggersInfo.Core.Replay.Numerics;
+global using System.Numerics;
