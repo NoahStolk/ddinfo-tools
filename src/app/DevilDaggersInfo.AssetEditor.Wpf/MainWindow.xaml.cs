6611b82b29e6eb6b9b6ff203e5672c98060ff46a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2022-06-01 21:16:09 +0200

    Restructure projects

---
+++ src/app/DevilDaggersInfo.AssetEditor.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+using DevilDaggersInfo.AssetEditor.Wpf.Services;
+using DevilDaggersInfo.Razor.Core.AssetEditor.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Windows;
+
+namespace DevilDaggersInfo.AssetEditor.Wpf;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		ServiceCollection serviceCollection = new();
+		serviceCollection.AddWpfBlazorWebView();
+		serviceCollection.AddSingleton<IErrorReporter, ErrorReporter>();
+		serviceCollection.AddSingleton<IFileSystemService, FileSystemService>();
+		serviceCollection.AddSingleton<BinaryState>();
+		Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+		InitializeComponent();
+
+		string? wpfVersion = Assembly.GetExecutingAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+		Title = $"Devil Daggers Asset Editor {wpfVersion}";
+		Width = SystemParameters.WorkArea.Width * 0.6;
+		Height = SystemParameters.WorkArea.Height * 0.6;
+	}
+}
---
+++ src/app/DevilDaggersInfo.AssetEditor.Wpf/Services/FileSystemService.cs
@@ -0,0 +1,73 @@
+using DevilDaggersInfo.Core.Asset.Enums;
+using DevilDaggersInfo.Core.Asset.Extensions;
+using DevilDaggersInfo.Razor.Core.AssetEditor.Services;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+using System.IO;
+
+namespace DevilDaggersInfo.AssetEditor.Wpf.Services;
+
+/// <summary>
+/// Platform-specific code for interacting with the OS file system.
+/// </summary>
+public class FileSystemService : IFileSystemService
+{
+	public string GetAssetTypeFilter(AssetType assetType)
+	{
+		if (assetType == AssetType.Shader)
+			throw new NotSupportedException($"Asset type '{AssetType.Shader}' has multiple file extensions.");
+
+		string fileTypeName = assetType switch
+		{
+			AssetType.Audio => "Audio",
+			AssetType.Mesh => "Mesh",
+			AssetType.ObjectBinding => "Text",
+			AssetType.Texture => "Texture",
+			_ => throw new NotSupportedException($"Asset type '{assetType}' is not supported."),
+		};
+
+		return BuildFilter(fileTypeName, false, $"*{assetType.GetFileExtension()}");
+	}
+
+	public string GetVertexShaderFilter() => BuildFilter("Vertex shader", true, "*.vert", "*.glsl");
+
+	public string GetFragmentShaderFilter() => BuildFilter("Fragment shader", true, "*.frag", "*.glsl");
+
+	private static string BuildFilter(string fileTypeName, bool includeAllFiles, params string[] fileExtensionPatterns)
+	{
+		string patterns = string.Join(";", fileExtensionPatterns);
+		return $"{fileTypeName} files ({patterns})|{patterns}{(includeAllFiles ? "|All files (*.*)|*.*" : null)}";
+	}
+
+	public IFileSystemService.FileResult? Open(string extensionFilter)
+	{
+		OpenFileDialog dialog = new()
+		{
+			Filter = extensionFilter,
+		};
+		bool? openResult = dialog.ShowDialog();
+		if (!openResult.HasValue || !openResult.Value)
+			return null;
+
+		return new(dialog.FileName, File.ReadAllBytes(dialog.FileName));
+	}
+
+	public void Save(byte[] buffer)
+	{
+		SaveFileDialog dialog = new();
+		bool? saveResult = dialog.ShowDialog();
+		if (!saveResult.HasValue || !saveResult.Value)
+			return;
+
+		File.WriteAllBytes(dialog.FileName, buffer);
+	}
+
+	public string? SelectDirectory()
+	{
+		VistaFolderBrowserDialog folderDialog = new();
+		if (folderDialog.ShowDialog() == true)
+			return folderDialog.SelectedPath;
+
+		return null;
+	}
+}
