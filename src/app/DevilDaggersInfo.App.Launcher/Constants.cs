5f2c2077edbe6321e53ad51e58c5a79c476eec1e
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2023-05-26 19:45:34 +0200

    Move app launcher to app directory

---
+++ src/app/DevilDaggersInfo.App.Launcher/Constants.cs
@@ -0,0 +1,17 @@
+namespace DevilDaggersInfo.App.Launcher;
+
+public static class Constants
+{
+#if WINDOWS
+	public const ToolBuildType AppBuildType = ToolBuildType.WindowsWarp;
+	public const ToolBuildType AppLauncherBuildType = ToolBuildType.WindowsConsole;
+#elif LINUX
+	public const ToolBuildType AppBuildType = ToolBuildType.LinuxWarp;
+	public const ToolBuildType AppLauncherBuildType = ToolBuildType.LinuxConsole;
+#endif
+
+	public const ToolPublishMethod AppPublishMethod = ToolPublishMethod.SelfContained; // TODO: What if we want to publish the app using native AOT?
+	public const ToolPublishMethod AppLauncherPublishMethod = ToolPublishMethod.Aot;
+
+	public const string InstallationDirectory = "DDINFO TOOLS";
+}
