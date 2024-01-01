2ddb0f80ca1b068da7061dab8ba7fb7ed3f30fdc
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-01 20:09:46 +0100

    Add DevUtil.BuildAssets project

---
+++ src/DevilDaggersInfo.DevUtil.BuildAssets/Program.cs
@@ -0,0 +1,10 @@
+﻿using DevilDaggersInfo.Tools.Engine.Content.Conversion;
+
+if (args.Length < 2)
+{
+	Console.WriteLine("Please provide the input root directory path and the output file path as arguments.");
+	Environment.Exit(1);
+	return;
+}
+
+ContentFileWriter.GenerateContentFile(args[0], args[1]);
