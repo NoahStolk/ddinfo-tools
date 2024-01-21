45b7aeb83d3476e544739a38302aac2c7040df73
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-21 13:53:25 +0100

    Implement compiling mods

--- src/DevilDaggersInfo.Tools/Ui/AssetEditor/CompileModWindow.cs
+++ src/DevilDaggersInfo.Tools/Ui/AssetEditor/CompileModWindow.cs
@@ -1,28 +1,58 @@
-using DevilDaggersInfo.Core.Mod;
+using DevilDaggersInfo.Core.Mod.Builders;
 using DevilDaggersInfo.Tools.EditorFileState;
 using DevilDaggersInfo.Tools.Ui.AssetEditor.Data;
+using DevilDaggersInfo.Tools.Ui.Popups;
+using DevilDaggersInfo.Tools.User.Settings;
 using ImGuiNET;
+using System.Diagnostics;
 
 namespace DevilDaggersInfo.Tools.Ui.AssetEditor;
 
 public static class CompileModWindow
 {
-	private static string? _outputDirectory;
+	private static string _outputDirectory = UserSettings.ModsDirectory;
+	private static string _outputFileName = "mod";
+
+	private static bool CreateAudio => FileStates.Mod.Object.Audio.Count > 0;
+	private static bool CreateDd => FileStates.Mod.Object.Meshes.Count > 0 || FileStates.Mod.Object.ObjectBindings.Count > 0 || FileStates.Mod.Object.Shaders.Count > 0 || FileStates.Mod.Object.Textures.Count > 0;
 
 	public static void Render()
 	{
 		if (ImGui.Begin("Compile mod"))
 		{
-			ImGui.Text(_outputDirectory ?? "No output path selected.");
+			ImGui.SeparatorText("File locations");
 			if (ImGui.Button("Browse"))
 			{
-				NativeFileDialog.SelectDirectory(s => _outputDirectory = s);
+				NativeFileDialog.SelectDirectory(s =>
+				{
+					if (s != null)
+						_outputDirectory = s;
+				});
 			}
 
-			if (ImGui.Button("Compile"))
+			ImGui.SameLine();
+			ImGui.Text(_outputDirectory);
+
+			ImGui.InputText("Output file name", ref _outputFileName, 64);
+
+			ImGui.SeparatorText("Compile");
+			if (CreateAudio || CreateDd)
 			{
-				// TODO: Show progress bar and hide button when compiling.
-				Task.Run(async () => await CompileAsync());
+				ImGui.Text("The following files will be created:");
+				if (CreateAudio)
+					ImGui.BulletText(Inline.Span($"audio{_outputFileName}"));
+				if (CreateDd)
+					ImGui.BulletText(Inline.Span($"dd{_outputFileName}"));
+
+				if (ImGui.Button("Compile"))
+				{
+					// TODO: Show progress bar and hide button when compiling.
+					Task.Run(async () => await CompileAsync());
+				}
+			}
+			else
+			{
+				ImGui.Text("No assets have been added to the mod.");
 			}
 		}
 
@@ -31,23 +61,94 @@ public static class CompileModWindow
 
 	private static async Task CompileAsync()
 	{
-		if (_outputDirectory == null)
+		if (!Directory.Exists(_outputDirectory))
+		{
+			PopupManager.ShowError("Output directory does not exist.");
 			return;
+		}
 
-		// TODO: Compile dd.
-		ModBinaryBuilder builder = new(ModBinaryType.Audio);
-		foreach (AudioAssetPath assetPath in FileStates.Mod.Object.Audio)
+		if (_outputFileName.Length == 0)
+		{
+			PopupManager.ShowError("File name cannot be empty.");
+			return;
+		}
+
+		if (_outputFileName.Any(c => Path.GetInvalidFileNameChars().Contains(c)))
+		{
+			PopupManager.ShowError("File name contains invalid characters.");
+			return;
+		}
+
+		if (File.Exists(Path.Combine(_outputDirectory, $"audio{_outputFileName}")))
+		{
+			PopupManager.ShowError($"File 'audio{_outputFileName}' already exists in the output directory.");
+			return;
+		}
+
+		if (File.Exists(Path.Combine(_outputDirectory, $"dd{_outputFileName}")))
+		{
+			PopupManager.ShowError($"File 'dd{_outputFileName}' already exists in the output directory.");
+			return;
+		}
+
+		AssetPaths mod = FileStates.Mod.Object;
+
+		if (CreateAudio)
+		{
+			byte[] audioBinary = await BuildAudioBinaryAsync(mod.Audio);
+			await File.WriteAllBytesAsync(Path.Combine(_outputDirectory, $"audio{_outputFileName}"), audioBinary);
+		}
+
+		if (CreateDd)
+		{
+			byte[] ddBinary = await BuildDdBinaryAsync(mod.Meshes.Cast<IAssetPath>().Concat(mod.ObjectBindings).Concat(mod.Shaders).Concat(mod.Textures).ToList());
+			await File.WriteAllBytesAsync(Path.Combine(_outputDirectory, $"dd{_outputFileName}"), ddBinary);
+		}
+	}
+
+	private static async Task<byte[]> BuildAudioBinaryAsync(List<AudioAssetPath> assetPaths)
+	{
+		AudioModBinaryBuilder builder = new();
+		foreach (AudioAssetPath assetPath in assetPaths)
 		{
 			if (assetPath.AbsolutePath == null)
 				continue;
 
 			byte[] fileContents = await File.ReadAllBytesAsync(assetPath.AbsolutePath);
-			builder.AddAsset(assetPath.AssetName, assetPath.AssetType, fileContents);
+			builder.AddAudio(assetPath.AssetName, fileContents);
 		}
 
-		byte[] modContents = builder.Compile();
+		return builder.Compile();
+	}
 
-		// TODO: Pick file name.
-		await File.WriteAllBytesAsync(Path.Combine(_outputDirectory, $"audio{FileStates.Mod.FileName}"), modContents);
+	private static async Task<byte[]> BuildDdBinaryAsync(List<IAssetPath> assetPaths)
+	{
+		DdModBinaryBuilder builder = new();
+		foreach (IAssetPath assetPath in assetPaths)
+		{
+			switch (assetPath)
+			{
+				case MeshAssetPath meshAssetPath:
+					if (meshAssetPath.AbsolutePath != null)
+						builder.AddMesh(meshAssetPath.AssetName, await File.ReadAllBytesAsync(meshAssetPath.AbsolutePath));
+					break;
+				case ObjectBindingAssetPath objectBindingAssetPath:
+					if (objectBindingAssetPath.AbsolutePath != null)
+						builder.AddObjectBinding(objectBindingAssetPath.AssetName, await File.ReadAllBytesAsync(objectBindingAssetPath.AbsolutePath));
+					break;
+				case ShaderAssetPath shaderAssetPath:
+					if (shaderAssetPath is { AbsoluteVertexPath: not null, AbsoluteFragmentPath: not null })
+						builder.AddShader(shaderAssetPath.AssetName, await File.ReadAllBytesAsync(shaderAssetPath.AbsoluteVertexPath), await File.ReadAllBytesAsync(shaderAssetPath.AbsoluteFragmentPath));
+					break;
+				case TextureAssetPath textureAssetPath:
+					if (textureAssetPath.AbsolutePath != null)
+						builder.AddTexture(textureAssetPath.AssetName, await File.ReadAllBytesAsync(textureAssetPath.AbsolutePath));
+					break;
+				default:
+					throw new UnreachableException();
+			}
+		}
+
+		return builder.Compile();
 	}
 }
