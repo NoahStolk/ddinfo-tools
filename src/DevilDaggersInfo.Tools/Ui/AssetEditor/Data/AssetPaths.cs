6b14fdd12399e3a9fa40d111c225270e34afa1c8
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-01-21 14:51:44 +0100

    Clean up mod file before saving

--- src/DevilDaggersInfo.Tools/Ui/AssetEditor/Data/AssetPaths.cs
+++ src/DevilDaggersInfo.Tools/Ui/AssetEditor/Data/AssetPaths.cs
@@ -23,6 +23,42 @@ public class AssetPaths
 
 	public byte[] ToJsonBytes()
 	{
+		for (int i = Audio.Count - 1; i >= 0; i--)
+		{
+			if (Audio[i].AbsolutePath == null && Audio[i].Loudness == null)
+				Audio.RemoveAt(i);
+		}
+
+		for (int i = Meshes.Count - 1; i >= 0; i--)
+		{
+			if (Meshes[i].AbsolutePath == null)
+				Meshes.RemoveAt(i);
+		}
+
+		for (int i = ObjectBindings.Count - 1; i >= 0; i--)
+		{
+			if (ObjectBindings[i].AbsolutePath == null)
+				ObjectBindings.RemoveAt(i);
+		}
+
+		for (int i = Shaders.Count - 1; i >= 0; i--)
+		{
+			if (Shaders[i].AbsoluteVertexPath == null && Shaders[i].AbsoluteFragmentPath == null)
+				Shaders.RemoveAt(i);
+		}
+
+		for (int i = Textures.Count - 1; i >= 0; i--)
+		{
+			if (Textures[i].AbsolutePath == null)
+				Textures.RemoveAt(i);
+		}
+
+		Audio.Sort((a, b) => string.CompareOrdinal(a.AssetName, b.AssetName));
+		Meshes.Sort((a, b) => string.CompareOrdinal(a.AssetName, b.AssetName));
+		ObjectBindings.Sort((a, b) => string.CompareOrdinal(a.AssetName, b.AssetName));
+		Shaders.Sort((a, b) => string.CompareOrdinal(a.AssetName, b.AssetName));
+		Textures.Sort((a, b) => string.CompareOrdinal(a.AssetName, b.AssetName));
+
 		return JsonSerializer.SerializeToUtf8Bytes(this, AssetPathsContext.Default.AssetPaths);
 	}
 }
