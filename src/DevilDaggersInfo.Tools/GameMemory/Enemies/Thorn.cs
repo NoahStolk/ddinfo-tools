e5cf245fc7a7438e7664201e5295523e24f6faba
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-02-28 20:57:47 +0100

    Find Spider2 list in memory

--- src/DevilDaggersInfo.Tools/GameMemory/Enemies/Thorn.cs
+++ src/DevilDaggersInfo.Tools/GameMemory/Enemies/Thorn.cs
@@ -4,7 +4,21 @@ namespace DevilDaggersInfo.Tools.GameMemory.Enemies;
 
 public record struct Thorn
 {
+	public ThornState State;
+	public int IsAlive;
+	public float StateTimer;
 	public int Hp;
 	public Vector3 Position;
-	public float Timer2;
+	public float Rotation;
+	public float Unknown1;
+	public float Unknown2;
+	public float Unknown3;
+	public float Unknown4;
+	public float Unknown5;
+	public float Unknown6;
+	public float Unknown7;
+	public float Unknown8;
+	public float Unknown9;
+	public float Unknown10;
+	public float Unknown11;
 }
