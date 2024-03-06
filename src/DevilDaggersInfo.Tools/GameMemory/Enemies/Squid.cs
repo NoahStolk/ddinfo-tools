a577da27ea54ecd6c56c9ab2deebf079dffe968d
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-03-06 22:30:34 +0100

    Add SquidType enum

--- src/DevilDaggersInfo.Tools/GameMemory/Enemies/Squid.cs
+++ src/DevilDaggersInfo.Tools/GameMemory/Enemies/Squid.cs
@@ -6,8 +6,9 @@ namespace DevilDaggersInfo.Tools.GameMemory.Enemies;
 [StructLayout(LayoutKind.Explicit, Size = StructSizes.Squid)]
 public record struct Squid
 {
-	[FieldOffset(0)] public int HpNode1;
-	[FieldOffset(4)] public int HpNode2;
-	[FieldOffset(8)] public int HpNode3;
+	[FieldOffset(148)] public int HpNode1;
+	[FieldOffset(152)] public int HpNode2;
+	[FieldOffset(156)] public int HpNode3;
+	[FieldOffset(240)] public SquidType Type;
 }
 #pragma warning restore SA1134
