03f720415d575961052a74df219dada2b8d084a3
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-03-08 19:57:05 +0100

    Find boid struct size

--- src/DevilDaggersInfo.Tools/GameMemory/Enemies/StructSizes.cs
+++ src/DevilDaggersInfo.Tools/GameMemory/Enemies/StructSizes.cs
@@ -7,5 +7,5 @@ public static class StructSizes
 	public const int Leviathan = 480;
 	public const int Squid = 608;
 	public const int Pede = 6984 + 50 * 352; // Guess
-	public const int Boid = 320; // Guess
+	public const int Boid = 520;
 }
