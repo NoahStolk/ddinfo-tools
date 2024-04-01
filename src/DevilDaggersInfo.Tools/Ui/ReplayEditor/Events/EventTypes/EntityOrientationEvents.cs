83f8a33c2e7648418d64fea264e8790f9511ae5a
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-04-01 19:59:00 +0200

    Re-add missing data UI

--- src/DevilDaggersInfo.Tools/Ui/ReplayEditor/Events/EventTypes/EntityOrientationEvents.cs
+++ src/DevilDaggersInfo.Tools/Ui/ReplayEditor/Events/EventTypes/EntityOrientationEvents.cs
@@ -6,15 +6,17 @@ namespace DevilDaggersInfo.Tools.Ui.ReplayEditor.Events.EventTypes;
 
 public sealed class EntityOrientationEvents : IEventTypeRenderer<EntityOrientationEventData>
 {
-	public static int ColumnCount => 1;
+	public static int ColumnCount => 2;
 
 	public static void SetupColumns()
 	{
+		ImGui.TableSetupColumn("Entity Id", ImGuiTableColumnFlags.WidthFixed, 160);
 		ImGui.TableSetupColumn("Orientation", ImGuiTableColumnFlags.WidthFixed, 384);
 	}
 
 	public static void Render(EntityOrientationEventData e, EditorReplayModel replay)
 	{
+		EventTypeRendererUtils.NextColumnEntityId(replay, e.EntityId);
 		EventTypeRendererUtils.NextColumn(e.Orientation);
 	}
 }
--- src/DevilDaggersInfo.Tools/Ui/ReplayEditor/Events/EventTypes/EntityPositionEvents.cs
+++ src/DevilDaggersInfo.Tools/Ui/ReplayEditor/Events/EventTypes/EntityPositionEvents.cs
@@ -6,15 +6,17 @@ namespace DevilDaggersInfo.Tools.Ui.ReplayEditor.Events.EventTypes;
 
 public sealed class EntityPositionEvents : IEventTypeRenderer<EntityPositionEventData>
 {
-	public static int ColumnCount => 1;
+	public static int ColumnCount => 2;
 
 	public static void SetupColumns()
 	{
+		ImGui.TableSetupColumn("Entity Id", ImGuiTableColumnFlags.WidthFixed, 160);
 		ImGui.TableSetupColumn("Position", ImGuiTableColumnFlags.WidthFixed, 128);
 	}
 
 	public static void Render(EntityPositionEventData e, EditorReplayModel replay)
 	{
+		EventTypeRendererUtils.NextColumnEntityId(replay, e.EntityId);
 		EventTypeRendererUtils.NextColumn(e.Position);
 	}
 }
