a63ff3dc40b28a10018b58dfa6223c63df3d6922
Noah Stolk <31079637+NoahStolk@users.noreply.github.com>
2024-05-15 21:59:32 +0200

    Add comment

--- src/DevilDaggersInfo.Tools/GameMemory/Enemies/Data/Squid.cs
+++ src/DevilDaggersInfo.Tools/GameMemory/Enemies/Data/Squid.cs
@@ -12,7 +12,7 @@ public record struct Squid
 	[FieldOffset(148)] public int NodeHp1;
 	[FieldOffset(152)] public int NodeHp2;
 	[FieldOffset(156)] public int NodeHp3;
-	[FieldOffset(172)] public float GushCountDown;
+	[FieldOffset(172)] public float GushCountDown; // Starts at -3, counts up to 0, then reset to -20. After player death, it doesn't reset and keeps increasing. TODO: Verify all of this.
 	[FieldOffset(240)] public SquidType Type;
 }
 #pragma warning restore SA1134
