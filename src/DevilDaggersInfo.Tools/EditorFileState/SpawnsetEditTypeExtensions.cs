5fc09955dbab3e768432ecf724614824c3b4a3a2
Noah Stolk <noah.stolk@gmail.com>
2026-08-20 15:58:45 +0200

    Use extension blocks

--- src/DevilDaggersInfo.Tools/EditorFileState/SpawnsetEditTypeExtensions.cs
+++ src/DevilDaggersInfo.Tools/EditorFileState/SpawnsetEditTypeExtensions.cs
@@ -15,63 +15,66 @@ internal static class SpawnsetEditTypeExtensions
 	private static readonly Color _colorPractice = new(0, _colorValue, _colorValue, 255);
 	private static readonly Color _colorSpawn = new(_colorValue, 0, _colorValue, 255);
 
-	public static ReadOnlySpan<byte> GetChange(this SpawnsetEditType spawnsetEditType)
+	extension(SpawnsetEditType spawnsetEditType)
 	{
-		return spawnsetEditType switch
+		public ReadOnlySpan<byte> GetChange()
 		{
-			SpawnsetEditType.Reset => "Spawnset reset"u8,
-			SpawnsetEditType.ArenaTileHeight => "Arena tile height edit"u8,
-			SpawnsetEditType.ArenaPencil => "Arena pencil edit"u8,
-			SpawnsetEditType.ArenaLine => "Arena line edit"u8,
-			SpawnsetEditType.ArenaRectangle => "Arena rectangle edit"u8,
-			SpawnsetEditType.ArenaEllipse => "Arena ellipse edit"u8,
-			SpawnsetEditType.ArenaBucket => "Arena bucket edit"u8,
-			SpawnsetEditType.RaceDagger => "Race dagger position change"u8,
-			SpawnsetEditType.ShrinkStart => "Shrink start change"u8,
-			SpawnsetEditType.ShrinkEnd => "Shrink end change"u8,
-			SpawnsetEditType.ShrinkRate => "Shrink rate change"u8,
-			SpawnsetEditType.Brightness => "Brightness change"u8,
-			SpawnsetEditType.Format => "Format change"u8,
-			SpawnsetEditType.GameMode => "Game mode change"u8,
-			SpawnsetEditType.HandLevel => "Hand level change"u8,
-			SpawnsetEditType.AdditionalGems => "Additional gems change"u8,
-			SpawnsetEditType.TimerStart => "Timer start change"u8,
-			SpawnsetEditType.SpawnDelete => "Spawn deletion"u8,
-			SpawnsetEditType.SpawnAdd => "Spawn addition"u8,
-			SpawnsetEditType.SpawnEdit => "Spawn edit"u8,
-			SpawnsetEditType.SpawnInsert => "Spawn insertion"u8,
-			SpawnsetEditType.SpawnsTransformation => "Spawns transformation"u8,
-			_ => throw new UnreachableException(),
-		};
-	}
+			return spawnsetEditType switch
+			{
+				SpawnsetEditType.Reset => "Spawnset reset"u8,
+				SpawnsetEditType.ArenaTileHeight => "Arena tile height edit"u8,
+				SpawnsetEditType.ArenaPencil => "Arena pencil edit"u8,
+				SpawnsetEditType.ArenaLine => "Arena line edit"u8,
+				SpawnsetEditType.ArenaRectangle => "Arena rectangle edit"u8,
+				SpawnsetEditType.ArenaEllipse => "Arena ellipse edit"u8,
+				SpawnsetEditType.ArenaBucket => "Arena bucket edit"u8,
+				SpawnsetEditType.RaceDagger => "Race dagger position change"u8,
+				SpawnsetEditType.ShrinkStart => "Shrink start change"u8,
+				SpawnsetEditType.ShrinkEnd => "Shrink end change"u8,
+				SpawnsetEditType.ShrinkRate => "Shrink rate change"u8,
+				SpawnsetEditType.Brightness => "Brightness change"u8,
+				SpawnsetEditType.Format => "Format change"u8,
+				SpawnsetEditType.GameMode => "Game mode change"u8,
+				SpawnsetEditType.HandLevel => "Hand level change"u8,
+				SpawnsetEditType.AdditionalGems => "Additional gems change"u8,
+				SpawnsetEditType.TimerStart => "Timer start change"u8,
+				SpawnsetEditType.SpawnDelete => "Spawn deletion"u8,
+				SpawnsetEditType.SpawnAdd => "Spawn addition"u8,
+				SpawnsetEditType.SpawnEdit => "Spawn edit"u8,
+				SpawnsetEditType.SpawnInsert => "Spawn insertion"u8,
+				SpawnsetEditType.SpawnsTransformation => "Spawns transformation"u8,
+				_ => throw new UnreachableException(),
+			};
+		}
 
-	public static Color GetColor(this SpawnsetEditType spawnsetEditType)
-	{
-		return spawnsetEditType switch
+		public Color GetColor()
 		{
-			SpawnsetEditType.Reset => _colorMisc,
-			SpawnsetEditType.ArenaTileHeight => _colorArena,
-			SpawnsetEditType.ArenaPencil => _colorArena,
-			SpawnsetEditType.ArenaLine => _colorArena,
-			SpawnsetEditType.ArenaRectangle => _colorArena,
-			SpawnsetEditType.ArenaEllipse => _colorArena,
-			SpawnsetEditType.ArenaBucket => _colorArena,
-			SpawnsetEditType.RaceDagger => _colorRaceDagger,
-			SpawnsetEditType.ShrinkStart => _colorShrink,
-			SpawnsetEditType.ShrinkEnd => _colorShrink,
-			SpawnsetEditType.ShrinkRate => _colorShrink,
-			SpawnsetEditType.Brightness => _colorBrightness,
-			SpawnsetEditType.Format => _colorMisc,
-			SpawnsetEditType.GameMode => _colorMisc,
-			SpawnsetEditType.HandLevel => _colorPractice,
-			SpawnsetEditType.AdditionalGems => _colorPractice,
-			SpawnsetEditType.TimerStart => _colorPractice,
-			SpawnsetEditType.SpawnDelete => _colorSpawn,
-			SpawnsetEditType.SpawnAdd => _colorSpawn,
-			SpawnsetEditType.SpawnEdit => _colorSpawn,
-			SpawnsetEditType.SpawnInsert => _colorSpawn,
-			SpawnsetEditType.SpawnsTransformation => _colorSpawn,
-			_ => throw new UnreachableException(),
-		};
+			return spawnsetEditType switch
+			{
+				SpawnsetEditType.Reset => _colorMisc,
+				SpawnsetEditType.ArenaTileHeight => _colorArena,
+				SpawnsetEditType.ArenaPencil => _colorArena,
+				SpawnsetEditType.ArenaLine => _colorArena,
+				SpawnsetEditType.ArenaRectangle => _colorArena,
+				SpawnsetEditType.ArenaEllipse => _colorArena,
+				SpawnsetEditType.ArenaBucket => _colorArena,
+				SpawnsetEditType.RaceDagger => _colorRaceDagger,
+				SpawnsetEditType.ShrinkStart => _colorShrink,
+				SpawnsetEditType.ShrinkEnd => _colorShrink,
+				SpawnsetEditType.ShrinkRate => _colorShrink,
+				SpawnsetEditType.Brightness => _colorBrightness,
+				SpawnsetEditType.Format => _colorMisc,
+				SpawnsetEditType.GameMode => _colorMisc,
+				SpawnsetEditType.HandLevel => _colorPractice,
+				SpawnsetEditType.AdditionalGems => _colorPractice,
+				SpawnsetEditType.TimerStart => _colorPractice,
+				SpawnsetEditType.SpawnDelete => _colorSpawn,
+				SpawnsetEditType.SpawnAdd => _colorSpawn,
+				SpawnsetEditType.SpawnEdit => _colorSpawn,
+				SpawnsetEditType.SpawnInsert => _colorSpawn,
+				SpawnsetEditType.SpawnsTransformation => _colorSpawn,
+				_ => throw new UnreachableException(),
+			};
+		}
 	}
 }
--- src/DevilDaggersInfo.Tools/Ui/ImGuiImage.cs
+++ src/DevilDaggersInfo.Tools/Ui/ImGuiImage.cs
@@ -6,25 +6,28 @@ namespace DevilDaggersInfo.Tools.Ui;
 
 internal static class ImGuiImage
 {
-	public static void AddFramebufferImage(this ImDrawListPtr drawListPtr, FramebufferData framebufferData, Vector2 pMin, Vector2 pMax)
+	extension(ImDrawListPtr drawListPtr)
 	{
-		drawListPtr.AddFramebufferImage(framebufferData, pMin, pMax, Color.White);
-	}
+		public void AddFramebufferImage(FramebufferData framebufferData, Vector2 pMin, Vector2 pMax)
+		{
+			drawListPtr.AddFramebufferImage(framebufferData, pMin, pMax, Color.White);
+		}
 
-	public static void AddFramebufferImage(this ImDrawListPtr drawListPtr, FramebufferData framebufferData, Vector2 pMin, Vector2 pMax, Color color)
-	{
-		// Framebuffers are flipped vertically, so we need to flip the UVs.
-		drawListPtr.AddImage(GetTextureRef(framebufferData.TextureHandle), pMin, pMax, Vector2.UnitY, Vector2.UnitX, ImGui.GetColorU32(color));
-	}
+		public void AddFramebufferImage(FramebufferData framebufferData, Vector2 pMin, Vector2 pMax, Color color)
+		{
+			// Framebuffers are flipped vertically, so we need to flip the UVs.
+			drawListPtr.AddImage(GetTextureRef(framebufferData.TextureHandle), pMin, pMax, Vector2.UnitY, Vector2.UnitX, ImGui.GetColorU32(color));
+		}
 
-	public static void AddImage(this ImDrawListPtr drawListPtr, uint imageId, Vector2 pMin, Vector2 pMax)
-	{
-		AddImage(drawListPtr, imageId, pMin, pMax, Color.White);
-	}
+		public void AddImage(uint imageId, Vector2 pMin, Vector2 pMax)
+		{
+			drawListPtr.AddImage(imageId, pMin, pMax, Color.White);
+		}
 
-	public static void AddImage(this ImDrawListPtr drawListPtr, uint imageId, Vector2 pMin, Vector2 pMax, Color color)
-	{
-		drawListPtr.AddImage(GetTextureRef(imageId), pMin, pMax, Vector2.Zero, Vector2.One, ImGui.GetColorU32(color));
+		public void AddImage(uint imageId, Vector2 pMin, Vector2 pMax, Color color)
+		{
+			drawListPtr.AddImage(GetTextureRef(imageId), pMin, pMax, Vector2.Zero, Vector2.One, ImGui.GetColorU32(color));
+		}
 	}
 
 	public static void Image(uint imageId, Vector2 size)
